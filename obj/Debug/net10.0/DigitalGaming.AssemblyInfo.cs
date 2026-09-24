5b61e1938e0835e038d345cc8577f3312d5f0da6
JuanReyesVazquez23 <reyesvjuandejesus@gmail.com>
2026-09-24 18:21:59 -0400

    fonts gamers implementadas

--- obj/Debug/net10.0/DigitalGaming.AssemblyInfo.cs
+++ obj/Debug/net10.0/DigitalGaming.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DigitalGaming")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3f8460db4a7db27f1ef53d11cfc3296ea44f4e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c51f9ea57e6f302d62ad7316f6615ee0a2d5ae46")]
 [assembly: System.Reflection.AssemblyProductAttribute("DigitalGaming")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DigitalGaming")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
